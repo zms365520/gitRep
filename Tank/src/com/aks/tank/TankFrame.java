60bb2d0535dbea3e85ac3ab27f3cc86f21d656c8
zhangms <949452420@qq.com>
2024-03-29 09:58:46 +0800

    根据按键确定坦克方向，根据方向确定坦克位移

--- Tank/src/com/aks/tank/TankFrame.java
+++ Tank/src/com/aks/tank/TankFrame.java
@@ -14,6 +14,8 @@ import java.awt.event.WindowEvent;
  */
 public class TankFrame extends Frame {
     int x = 200, y = 200;
+    Dir dir = Dir.DOWN;
+    private static final int SPEED = 10;
 
     public TankFrame() {
 
@@ -40,16 +42,32 @@ public class TankFrame extends Frame {
         g.fillRect(x, y, 50, 50);
         //x+=10;
         //y+=10;
+
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+        }
+
     }
 
     //键盘监听的处理类
     class MyKeyListener extends KeyAdapter {
 
         //设置四个方向是否被按下  false表示未按下
-        boolean bL=false;
-        boolean bR=false;
-        boolean bU=false;
-        boolean bD=false;
+        boolean bL = false;
+        boolean bR = false;
+        boolean bU = false;
+        boolean bD = false;
 
         //键盘按下，会触发该方法
         @Override
@@ -64,26 +82,27 @@ public class TankFrame extends Frame {
                 //左键被按下
                 case KeyEvent.VK_LEFT:
                     //x -= 10;
-                    bL=true;
+                    bL = true;
                     break;
                 //上键被按下
                 case KeyEvent.VK_UP:
                     //y -= 10;
-                    bU=true;
+                    bU = true;
                     break;
                 //右键被按下
                 case KeyEvent.VK_RIGHT:
                     //x += 10;
-                    bR=true;
+                    bR = true;
                     break;
                 //下键被按下
                 case KeyEvent.VK_DOWN:
                     //y += 10;
-                    bD=true;
+                    bD = true;
                     break;
                 default:
                     break;
             }
+            setMainTankDir();
         }
 
         //键盘被抬起，会触发该方法
@@ -97,23 +116,40 @@ public class TankFrame extends Frame {
             switch (key) {
                 //左键被按下
                 case KeyEvent.VK_LEFT:
-                    bL=false;
+                    bL = false;
                     break;
                 //上键被按下
                 case KeyEvent.VK_UP:
-                    bU=false;
+                    bU = false;
                     break;
                 //右键被按下
                 case KeyEvent.VK_RIGHT:
-                    bR=false;
+                    bR = false;
                     break;
                 //下键被按下
                 case KeyEvent.VK_DOWN:
-                    bD=false;
+                    bD = false;
                     break;
                 default:
                     break;
             }
+            setMainTankDir();
         }
+
+        private void setMainTankDir() {
+            if (bL) {
+                dir = Dir.LEFT;
+            }
+            if (bR) {
+                dir = Dir.RIGHT;
+            }
+            if (bU) {
+                dir = Dir.UP;
+            }
+            if (bD) {
+                dir = Dir.DOWN;
+            }
+        }
+
     }
 }
