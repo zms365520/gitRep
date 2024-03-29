e3f7c6d2b138b405e1c57bb24e0b415d3603ab8f
zhangms <949452420@qq.com>
2024-03-29 09:44:06 +0800

    定义枚举类型的四个方向

---
+++ Tank/src/com/aks/tank/Dir.java
@@ -0,0 +1,12 @@
+package com.aks.tank;
+
+/**
+ * @Auther: zms
+ * @Date: 2024/3/29 - 03 - 29 - 9:42
+ * @Description: com.aks.tank
+ * @version: 1.0
+ */
+public enum Dir {
+    //定义四个方向。（也可以定义成八个方向）
+    LEFT,UP,RIGHT,DOWN
+}
