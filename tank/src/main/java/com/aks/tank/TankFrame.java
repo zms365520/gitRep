c0b6b853bcc3641e4b63285370ff1326935e2301
zhangms <949452420@qq.com>
2024-11-15 13:55:26 +0800

    修改TankFrame代码的写法，效果和上面的是一样的

--- tank/src/main/java/com/aks/tank/TankFrame.java
+++ tank/src/main/java/com/aks/tank/TankFrame.java
@@ -7,13 +7,13 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
     public TankFrame(){
 
-        this.setSize(800,600);//设置大小
-        this.setResizable(false);//窗口不可改变大小
-        this.setTitle("Tank War another");
-        this.setVisible(true);
+        setSize(800,600);//设置大小
+        setResizable(false);//窗口不可改变大小
+        setTitle("Tank War another");
+        setVisible(true);
 
         //f的window监听器，监听者windowClosing这件事，当点击❌时，系统退出
-        this.addWindowListener(new WindowAdapter() {
+        addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
