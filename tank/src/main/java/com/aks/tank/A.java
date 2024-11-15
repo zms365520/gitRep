4f69a88aad2b0a9bf8514505de5417d1a0fc87b6
zhangms <949452420@qq.com>
2024-11-15 16:17:50 +0800

    测试tank仓库

---
+++ tank/src/main/java/com/aks/tank/A.java
@@ -0,0 +1,11 @@
+package com.aks.tank;
+
+/**
+ * @Auther: zms
+ * @Date: 2024/11/15 - 11 - 15 - 16:17
+ * @Description: com.aks.tank
+ * @version: 1.0
+ */
+public class A {
+    private int num;
+}
